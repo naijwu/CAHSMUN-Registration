2b9affd504138eba8627e59e2b800125b50fb6dc
Jae Wu <jaewuchun@gmail.com>
2020-08-07 01:22:32 -0700

    added rooming binding and unbinding, similar to assignments

--- src/main/java/org/cahsmun/registration/rooming/RoomingController.java
+++ src/main/java/org/cahsmun/registration/rooming/RoomingController.java
@@ -2,6 +2,8 @@ package org.cahsmun.registration.rooming;
 
 import lombok.extern.slf4j.Slf4j;
 import org.cahsmun.registration.authentication.UserExistException;
+import org.cahsmun.registration.delegate.Delegate;
+import org.cahsmun.registration.delegate.DelegateRepository;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,9 @@ public class RoomingController {
 
     @Resource
     RoomingRepository roomingRepository;
+
+    @Resource
+    DelegateRepository delegateRepository;
 
     @GetMapping("/rooms")
     public List<Rooming> retrieveAllRoomings() {
@@ -48,6 +53,58 @@ public class RoomingController {
 
     @PostMapping("/rooms")
     public Rooming createDelegation(@Valid @RequestBody Rooming rooming) throws UserExistException {
+        return roomingRepository.save(rooming);
+    }
+
+    @PostMapping("/room/bind/{room_id}/{room_slot}/{delegate_id}")
+    public Rooming bindRooming(@PathVariable long room_id, @PathVariable long delegate_id, @PathVariable int room_slot) {
+        Rooming rooming = roomingRepository.findById(room_id).orElseThrow(() -> new ResourceNotFoundException("Rooming not found with ID: " + room_id));
+        Delegate delegate = delegateRepository.findById(delegate_id).orElseThrow(() -> new ResourceNotFoundException("Delegate not found with ID: " + delegate_id));
+
+        switch (room_slot) {
+            case 1:
+                rooming.setDelegate_one(delegate.getDelegate_id());
+                break;
+            case 2:
+                rooming.setDelegate_two(delegate.getDelegate_id());
+                break;
+            case 3:
+                rooming.setDelegate_three(delegate.getDelegate_id());
+                break;
+            case 4:
+                rooming.setDelegate_four(delegate.getDelegate_id());
+                break;
+        }
+
+        delegate.setRooming_id(rooming.getRooming_id());
+
+        delegateRepository.save(delegate);
+        return roomingRepository.save(rooming);
+    }
+
+    @PostMapping("/room/unbind/{room_id}/{room_slot}/{delegate_id}")
+    public Rooming unbindRooming(@PathVariable long room_id, @PathVariable long delegate_id, @PathVariable int room_slot) {
+        Rooming rooming = roomingRepository.findById(room_id).orElseThrow(() -> new ResourceNotFoundException("Rooming not found with ID: " + room_id));
+        Delegate delegate = delegateRepository.findById(delegate_id).orElseThrow(() -> new ResourceNotFoundException("Delegate not found with ID: " + delegate_id));
+
+        switch (room_slot) {
+            case 1:
+                rooming.setDelegate_one(0);
+                break;
+            case 2:
+                rooming.setDelegate_two(0);
+                break;
+            case 3:
+                rooming.setDelegate_three(0);
+                break;
+            case 4:
+                rooming.setDelegate_four(0);
+                break;
+        }
+
+        delegate.setRooming_id(0);
+
+        delegateRepository.save(delegate);
         return roomingRepository.save(rooming);
     }
 }
