881631b9a14966b91efb26cea34d4989dbf69409
Jae Wu <jaewuchun@gmail.com>
2020-08-22 17:40:14 -0700

    made another oopsie

--- src/main/java/org/cahsmun/registration/emails/EmailService.java
+++ src/main/java/org/cahsmun/registration/emails/EmailService.java
@@ -22,7 +22,7 @@ public class EmailService {
         Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
         Mail mail = new Mail(from, subject, to, content);
 
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        SendGrid sg = new SendGrid(SENDGRID_API);
         Request request = new Request();
         try {
             request.setMethod(Method.POST);
