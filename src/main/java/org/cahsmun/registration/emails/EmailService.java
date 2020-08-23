208b8458a47080f2d06728d98a52c769d2c7e2d6
Jae Wu <jaewuchun@gmail.com>
2020-08-22 21:19:37 -0700

    test 6

--- src/main/java/org/cahsmun/registration/emails/EmailService.java
+++ src/main/java/org/cahsmun/registration/emails/EmailService.java
@@ -116,8 +116,8 @@ public class EmailService {
         json.addProperty("email", "jaewuchun@gmail.com");
         json.addProperty("full_name", "Example Name");
 
-        personalization.addCustomArg("dynamic_template_data", "[" + json.toString() + "]");
-        personalization.addTo(new Email("jaewuchun@gmail.com"));
+        personalization.addCustomArg("dynamic_template_data", json.toString());
+        personalization.addTo(new Email(to_email));
 
         mail.addPersonalization(personalization);
 
